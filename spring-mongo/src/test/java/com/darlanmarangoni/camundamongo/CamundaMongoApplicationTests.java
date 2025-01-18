ee6c019e160333cddaa472f17243af5d249e54ea
Darlan Marangoni <marangoni.darlan1@gmail.com>
2025-01-18 18:34:45 -0300

    fix: rename project to spring-mongo

--- spring-mongo/src/test/java/com/darlanmarangoni/camundamongo/CamundaMongoApplicationTests.java
+++ spring-mongo/src/test/java/com/darlanmarangoni/camundamongo/CamundaMongoApplicationTests.java
@@ -1,13 +1,8 @@
 package com.darlanmarangoni.camundamongo;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CamundaMongoApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
