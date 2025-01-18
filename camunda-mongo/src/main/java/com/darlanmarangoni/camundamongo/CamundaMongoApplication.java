174b5e6ab4de0ff87d115ac2d297f67e6a756496
Darlan Marangoni <marangoni.darlan1@gmail.com>
2025-01-18 17:45:50 -0300

    feat: create camunda-mongo project

---
+++ camunda-mongo/src/main/java/com/darlanmarangoni/camundamongo/CamundaMongoApplication.java
@@ -0,0 +1,13 @@
+package com.darlanmarangoni.camundamongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamundaMongoApplication.class, args);
+    }
+
+}
---
+++ camunda-mongo/src/test/java/com/darlanmarangoni/camundamongo/CamundaMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darlanmarangoni.camundamongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
